9bc344bf8c5f71b72079c7c879a21ad68fb3b3f0
Khoi Bui <bkkhoi1800151@student.ctuet.edu.vn>
2021-04-25 22:15:06 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -226,7 +226,7 @@ function callSendAPI(sender_psid, response) {
     }, (err, res, body) => {
         
         if (!err) {
-            console.log('message sent!' + body)
+            console.log('message sent!' + res)
         } else {
             console.error("Unable to send message:" + err);
         }
